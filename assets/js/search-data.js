c9d1da24827fda47da8eb2e6ebd4c0cb771c1f32
chenhui07c8 <hui.chen@chalmers.se>
2025-02-28 14:54:44 +0000

    Deploying to gh-pages from @ chenhui07c8/chenhui07c8.github.io@4e8734da280b1987239c8c0557e55e95815cdeb3 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -78,28 +78,28 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%68%75%69.%63%68%65%6E@%63%68%61%6C%6D%65%72%73.%73%65", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-linkedin',
+        title: 'LinkedIn',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://www.linkedin.com/in/chenhui07c8", "_blank");
         },
       },{
-        id: 'social-rss',
-        title: 'RSS Feed',
+        id: 'social-orcid',
+        title: 'ORCID',
         section: 'Socials',
         handler: () => {
-          window.open("/feed.xml", "_blank");
+          window.open("https://orcid.org/0000-0002-9345-9966", "_blank");
         },
       },{
         id: 'social-scholar',
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://scholar.google.com/citations?user=i8_ipXIAAAAJ", "_blank");
         },
       },{
         id: 'social-custom_social',
